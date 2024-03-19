b0aac721818ee5b6c92cb94c1bffbe9e72c963f5
June <80965658+0147123@users.noreply.github.com>
2024-03-19 18:15:50 +0800

    Update setting.tsx

--- src/pages/setting/setting.tsx
+++ src/pages/setting/setting.tsx
@@ -54,138 +54,107 @@ const Setting: React.FunctionComponent<ISettingProps> = (props) => {
               <img className="icon-ionic-ios-arrow-forward-inactive" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
               <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
             </div>
-            <Link
-              to={`https://${process.env.REACT_APP_NETWORK === "Signum" ? "explorer.signum.network/address" : "testnet.explorer.signum.network/address"}/${userAccountId}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="x16186-VD8XDI">
-                <div className="x31">
-                  <div className="invite-friends-MxX0D6 inter-normal-white-15px">Account Id</div>
-                </div>
+            <Link to={`https://${process.env.REACT_APP_NETWORK === "Signum" ? "explorer.signum.network/address": "testnet.explorer.signum.network/address"}/${userAccountId}`} target="_blank" rel="noopener noreferrer">
+
+            <div className="x16186-VD8XDI">
+              <div className="x31">
+                <div className="invite-friends-MxX0D6 inter-normal-white-15px">Account Id</div>
+              </div>
                 <div className="x16184-QxO6IE x16184">
                   <div className="x12345678-lPJi7x inter-normal-white-15px ">{userAccountId}</div>
                 </div>
-                </Link>
-            <img
-              className="icon-ionic-ios-arrow-forward-inactive"
-              src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-              alt="Icon ionic-ios-arrow-forward"
-              />
-            <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
-          </div>
-          <div className="x16187-VD8XDI">
-            <div className="x31">
-              <div className="wallet-HTBhsQ inter-normal-white-15px">Wallet</div>
-            </div>
-            <div className="x16184-a64QJA x16184">
-              <div className="xt-wallet-u2WLwG inter-normal-white-15px brightness-0-5">XT wallet</div>
-            </div>
-            <img
-              className="icon-ionic-ios-arrow-forward-inactive"
-              src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-              alt="Icon ionic-ios-arrow-forward"
-              />
-            <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
-          </div>
-          <div className="x16189-VD8XDI">
-            <div className="x31">
-              <div className="multi-chain-switch-tbbzDm inter-normal-white-15px">Nodes</div>
-            </div>
-            <div className="x16184-6XzBK0 x16184">
-              <div className="signum-qeaPIP inter-normal-white-15px brightness-0-5">Selection</div>
-            </div>
-            <Link to="https://www.signumswap.com/tokens/5453974739826751020">
-              <img
-                className="icon-ionic-ios-arrow-forward-inactive"
-                src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-                alt="Icon ionic-ios-arrow-forward"
-                />
+                <img className="icon-ionic-ios-arrow-forward" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
               <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
+            </div>
+            </Link>
+
+            <div className="x16187-VD8XDI">
+              <div className="x31">
+                <div className="wallet-HTBhsQ inter-normal-white-15px">Wallet</div>
+              </div>
+              <div className="x16184-a64QJA x16184">
+                <div className="xt-wallet-u2WLwG inter-normal-white-15px brightness-0-5">XT wallet</div>
+              </div>
+              <img className="icon-ionic-ios-arrow-forward-inactive" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
+              <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
+            </div>
+            <div className="x16189-VD8XDI">
+              <div className="x31">
+                <div className="multi-chain-switch-tbbzDm inter-normal-white-15px">Nodes</div>
+              </div>
+              <div className="x16184-6XzBK0 x16184">
+                <div className="signum-qeaPIP inter-normal-white-15px brightness-0-5">Selection</div>
+              </div>
+              <Link to="https://www.signumswap.com/tokens/5453974739826751020" target="_blank" rel="noopener noreferrer">
+                <img className="icon-ionic-ios-arrow-forward-inactive" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
+                <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
               </Link>
-          </div>
-          <div className="x16190-VD8XDI">
-            <div className="x31">
-              <div className="nf-ts-marketplace-aBwqob inter-normal-white-15px ">NFTs Marketplace</div>
             </div>
-            <img
-              className="icon-ionic-ios-arrow-forward brightness-0-5"
-              src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-              alt="Icon ionic-ios-arrow-forward"
-              />
-            <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
-          </div>
-          <div className="x16199-VD8XDI">
-            <div className="x31">
-              <div className="get-sig-dao-token-R3QCCH inter-normal-white-15px ">Get SIGDAO Token</div>
+            <div className="x16190-VD8XDI">
+              <div className="x31">
+                <div className="nf-ts-marketplace-aBwqob inter-normal-white-15px ">NFTs Marketplace</div>
+              </div>
+              <img className="icon-ionic-ios-arrow-forward brightness-0-5" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
+              <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
             </div>
-            <img
-              className="icon-ionic-ios-arrow-forward brightness-0-5"
-              src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-              alt="Icon ionic-ios-arrow-forward"
-              />
-            <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
-          </div>
-          <Link to="https://www.bettermi.io/en/termsofservice.html">
-          <div className="x16192-VD8XDI">
-            <div className="x31">
-              <div className="terms-of-use-vMQ5ls inter-normal-white-15px">Terms of Service</div>
+            <div className="x16199-VD8XDI">
+              <div className="x31">
+                <div className="get-sig-dao-token-R3QCCH inter-normal-white-15px ">Get SIGDAO Token</div>
+              </div>
+              <img className="icon-ionic-ios-arrow-forward brightness-0-5" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
+              <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
             </div>
-            <img
-              className="icon-ionic-ios-arrow-forward"
-              src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-              alt="Icon ionic-ios-arrow-forward"
-              />
-            <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
-          </div>
-          </Link>
-          <Link to="https://www.bettermi.io/en/privacypolicy.html">
-          <div className="x16193-VD8XDI">
-            <div className="x31">
-              <div className="privacy-policy-Am8TnD inter-normal-white-15px">Privacy Policy</div>
-            </div>
-            <img
-              className="icon-ionic-ios-arrow-forward"
-              src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-              alt="Icon ionic-ios-arrow-forward"
-              />
-            <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
-          </div>
-          </Link>
+            <Link to="https://www.bettermi.io/en/termsofservice.html" target="_blank" rel="noopener noreferrer">
+              <div className="x16192-VD8XDI">
+                <div className="x31">
+                  <div className="terms-of-use-vMQ5ls inter-normal-white-15px">Terms of Service</div>
+                </div>
+                <img className="icon-ionic-ios-arrow-forward" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
+                <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
+              </div>
+            </Link>
+            <Link to="https://www.bettermi.io/en/privacypolicy.html" target="_blank" rel="noopener noreferrer">
+              <div className="x16193-VD8XDI">
+                <div className="x31">
+                  <div className="privacy-policy-Am8TnD inter-normal-white-15px">Privacy Policy</div>
+                </div>
+                <img className="icon-ionic-ios-arrow-forward" src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
+                <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
+              </div>
+            </Link>
+            <Link to="https://www.bettermi.io/index.html" target="_blank" rel="noopener noreferrer">
+
             <div className="x16223-VD8XDI">
               <div className="x31">
                 <div className="help-YkYlbF inter-normal-white-15px">Help</div>
               </div>
-              <Link to = "https://www.bettermi.io/index.html">
-              <img
-                className="icon-ionic-ios-arrow-forward "
-                src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png"
-                alt="Icon ionic-ios-arrow-forward"
-                />
-              <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
-              </Link>
+                <img className="icon-ionic-ios-arrow-forward " src="img/setting/icon-ionic-ios-arrow-forward-1@1x.png" alt="Icon ionic-ios-arrow-forward" />
+                <img className="x15" src="img/setting/file---15@1x.png" alt="15" />
             </div>
-          <div className="x16222-VD8XDI">
-            <div className="x16184-OCPXoe x16184">
-              <div className="x0004-pzNHOH inter-normal-white-15px">0.0.04</div>
-            </div>
-            <div className="x31">
-              <div className="version-xPcu6a inter-normal-white-15px">Version</div>
+            </Link>
+
+            <div className="x16222-VD8XDI">
+              <div className="x16184-OCPXoe x16184">
+                <div className="x0004-pzNHOH inter-normal-white-15px">0.0.04</div>
+              </div>
+              <div className="x31">
+                <div className="version-xPcu6a inter-normal-white-15px">Version</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="settings-xeBDcv settings inter-semi-bold-white-18px">Settings</div>
-      <a href="javascript:history.back()">
-        <div className="icon-arrow-left-7DUKxJ icon-arrow-left">
-          <img
-            className="icon-arrow-left-trdL9a icon-arrow-left"
-            src="img/setting/icon-arrow-left-10@1x.png"
-            alt="icon-arrow-left"
-            />
-        </div>
-      </a>
-        <Button className="bottom-controls-7DUKxJ" onClick = {() => {logout()}}>
+        <div className="settings-xeBDcv settings inter-semi-bold-white-18px">Settings</div>
+        <a href="javascript:history.back()">
+          <div className="icon-arrow-left-7DUKxJ icon-arrow-left">
+            <img className="icon-arrow-left-trdL9a icon-arrow-left" src="img/setting/icon-arrow-left-10@1x.png" alt="icon-arrow-left" />
+          </div>
+        </a>
+        <Button
+          className="bottom-controls-7DUKxJ"
+          onClick={() => {
+            logout();
+          }}
+        >
           <div className="button_-logout-ChdhUr">
             <div className="button1-GdXCBJ"></div>
             <div className="mintagain-GdXCBJ inter-semi-bold-white-15px">Logout</div>
