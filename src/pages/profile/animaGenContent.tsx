d5f6a4beeebb59f68c7a786a277a85bb1835924c
June <80965658+0147123@users.noreply.github.com>
2024-04-23 18:02:52 +0800

    Update animaGenContent.tsx

--- src/pages/profile/animaGenContent.tsx
+++ src/pages/profile/animaGenContent.tsx
@@ -27,10 +27,9 @@ import { UpdateUserDescription } from "../../NftSystem/updateUserNftStorage";
 import HorizontalScrollContainer from "../../components/horizontalScrollContainer";
 import IPFSImageComponent from "../../components/ipfsImgComponent";
 
-
-interface myNftProfile{
-  imageAddress:string,
-  rewardPercentage:string,
+interface myNftProfile {
+  imageAddress: string;
+  rewardPercentage: string;
 }
 interface IAnimaGenContentProps {
   isOpen: boolean;
@@ -44,7 +43,7 @@ interface IAnimaGenContentProps {
   setIsNFTiconLoading: Function;
   isNFTiconLoading?: boolean;
   setImgAddress: Function;
-  setRewardPercentage:(rewardPercentage:string) => void;
+  setRewardPercentage: (rewardPercentage: string) => void;
 }
 interface myNftList {
   level: string;
@@ -66,7 +65,7 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
   const dispatch = useDispatch();
   const gender = useSelector(selectCurrentGender);
   const userAccountId = useSelector(accountId);
-  const { isOpen, setIsOpen, isBackButton, setIsBackButton, isPopUpIcon, setIsPopUpIcon, isNFTiconLoading, setIsNFTiconLoading, setImgAddress ,setRewardPercentage} = props;
+  const { isOpen, setIsOpen, isBackButton, setIsBackButton, isPopUpIcon, setIsPopUpIcon, isNFTiconLoading, setIsNFTiconLoading, setImgAddress, setRewardPercentage } = props;
   const [name, setName] = useState<string>(username);
   const [haveNft, setHaveNft] = useState<boolean>(false);
   const [aboutYourselfText, setAboutYourselfText] = useState<string>(aboutYourself);
@@ -98,7 +97,7 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
         accountId: userAccountId,
       });
       let newDes = {};
-      newDes = Object.assign(newDes, {vs: 1});
+      newDes = Object.assign(newDes, { vs: 1 });
       newDes = Object.assign(newDes, { nm: name });
       newDes = Object.assign(newDes, { ds: aboutYourselfText });
       newDes = Object.assign(newDes, { hp: descriptionText });
@@ -110,7 +109,6 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
   };
 
   const handleSave = async () => {
-
     if (name.length === 0 || aboutYourselfText.length === 0 || descriptionText.length === 0 || discordUsernameText.length === 0) {
       setShowStar(true);
       return;
@@ -146,12 +144,9 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
   var userNftList: myNftProfile[] = [];
   const loadNftList = async () => {
     try {
-
       userNftList = await GetUserNftList(ledger2, userAccountId, nftDistributor, codeHashIdForNft);
       setMyNfts(userNftList);
       setLoadingNft(false);
-
-
     } catch (e: any) {
       console.log(e);
     }
@@ -159,7 +154,6 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
 
   useEffect(() => {
     if (nftLoaded.current === true) {
-
     } else {
       nftLoaded.current = true;
       loadNftList();
@@ -167,7 +161,6 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
   }, [nftContractStorage]);
 
   const handleScroll = (event: any) => {
-
     const container = document.querySelector("div.profileHorizontalScroll")!;
     const scrollAmount = event.deltaY;
 
@@ -196,17 +189,13 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
       ledger2.account
         .getAccount({ accountId: userAccountId })
         .then((account) => {
-
           const description = JSON.parse(account.description);
-
-
 
           setImgAddress(Object.keys(description.av)[0]);
           setIsNFTiconLoading(false);
         })
         .catch((error) => {
           setIsNFTiconLoading(false);
-
         });
     }
   };
@@ -233,7 +222,6 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
   /*Ends here*/
 
   const handleScroll2 = (event: any) => {
-
     const container = event.target!;
     const scrollAmount = event.deltaY;
     container.scrollTo({
@@ -272,31 +260,23 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
     let newDes = waitingToBeChangedDescription.description === undefined ? {} : JSON.parse(waitingToBeChangedDescription.description);
 
     if (newDes.nm == null) {
-
       setFetchName("");
     } else {
-
       setFetchName(newDes.nm);
     }
     if (newDes.ds == null) {
-
       setFetchDescription("");
     } else {
-
       setFetchDescription(newDes.ds);
     }
     if (newDes.sc == null) {
-
       setFetchDiscordUsername("");
     } else {
-
       setFetchDiscordUsername(newDes.sc[0]);
     }
     if (newDes.hp == null) {
-
       setFetchAboutYourself("");
     } else {
-
       setFetchAboutYourself(newDes.hp);
     }
     setIsSettingLoading(false);
@@ -313,7 +293,7 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
       style={{
         height: `${isOpen ? "100vh" : "844px"}`,
       }}
-      onWheel={handleScroll2}
+      // onWheel={handleScroll2}
     >
       <ShortTitleBar title="Profile" />
       {alert && (
@@ -324,47 +304,36 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
 
       <div className="overlap-group5">
         <div className="overlap-design-layout">
-          <div className="overlap-group1-profile">
-          <div className="profile-description-bg"></div>
-
-            {/* <img className="layer" src="img/profile/layer-1@1x.png" alt="Layer" /> */}
-            <Link to="/indexMyNftList">
-              <div className="button_nft-collections">
-                <div className="continue-profile inter-semi-bold-white-15px">My NFT Collection</div>
-              </div>
-            </Link>
-            <div className="ic_edit" onClick={() => setIsOpen(!isOpen)}>
-              <img className="ic_edit-content" src="img/profile/ic-edit-1@1x.png" alt="" />
-            </div>
-            
+          <div className="profile-description-bg">
             {isUpdatingUserSetting === true || isSettingLoading === true ? (
-            <>
-              <div className="profile_icon_nft_-avatar_empty">
-                <img className="profile_icon_ic_loading" src="/img/loadingMinting/mimi-dancing-for-loadin-page.gif" alt="ic_add" />
-              </div>
-
-            <div className="profile-content-container">
-              <div className="profile-content">
-                <div className="lds-ring">
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
+              <>
+                <div className="profile_icon_nft_-avatar_empty">
+                  <img className="profile_icon_ic_loading" src="/img/loadingMinting/mimi-dancing-for-loadin-page.gif" alt="ic_add" />
                 </div>
-              </div>
-              </div>
-            </>
+
+                <div className="profile-content-container">
+                  <div className="profile-content">
+                    <div className="lds-ring">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                  </div>
+                </div>
+              </>
             ) : (
               <>
-              <UserIcon setIsPopUpIcon={setIsPopUpIcon} profile={true} userAccountId={userAccountId} setRewardPercentage={ setRewardPercentage} setEnlargeImageAddress={setImgAddress}></UserIcon>
-              <div className="profile-content-container">
+                <UserIcon setIsPopUpIcon={setIsPopUpIcon} profile={true} userAccountId={userAccountId} setRewardPercentage={setRewardPercentage} setEnlargeImageAddress={setImgAddress}></UserIcon>
+                {/* <div className="profile-content-container">
+              </div> */}
                 <div className="profile-content">
                   <div className="zoe_li">{fetchName ? fetchName : name || "Enter your name"}</div>
                   <div className="perso-container">
+                    <p className="x29-personal-trainer inter-semi-bold-keppel-15px">{fetchAboutYourself ? fetchAboutYourself : aboutYourselfText || `♉️  |  29  |  PERSONAL TRAINER`}</p>
                     <p className="im-a-positive-perso" style={description ? {} : { color: "#8e8e8e" }}>
                       {fetchDescription ? fetchDescription : descriptionText || "Please enter DESCRIPTION TO FRIENDS"}
                     </p>
-                    <p className="x29-personal-trainer inter-semi-bold-keppel-15px">{fetchAboutYourself ? fetchAboutYourself : aboutYourselfText || `♉️  |  29  |  PERSONAL TRAINER`}</p>
                   </div>
                 </div>
                 <div className="x16227">
@@ -379,11 +348,17 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
                     <img src="img/profile/file---11690@1x.png" alt="" />
                   </div>
                 </div>
-              </div>
               </>
             )}
+            <Link to="/indexMyNftList">
+              <div className="button_nft-collections">
+                <div className="continue-profile inter-semi-bold-white-15px">My NFT Collection</div>
+              </div>
+            </Link>
+            <div className="ic_edit" onClick={() => setIsOpen(!isOpen)}>
+              <img className="ic_edit-content" src="img/profile/ic-edit-1@1x.png" alt="" />
+            </div>
           </div>
-
           {/* This is the new horizontal scroll */}
           <HorizontalScrollContainer
             inputClassName="profileHorizontalScroll"
@@ -404,7 +379,7 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
               // onWheel={handleScroll}
             >
               <Link to="/allNftList/">
-                <img className="profileBuyNft" src = "img/profile/NftMarketplaceBanner.png">
+                <img className="profileBuyNft" src="img/profile/NftMarketplaceBanner.png">
                   {/* <img className="ic_add" src="img/profile/ic-add-2@1x.png" alt="ic_add" /> */}
                   {/* <p className="inter-semi-bold-white-12px ">Buy NFT</p> */}
                 </img>
@@ -503,11 +478,6 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
                   width={300}
                   // ref={(el) => (inputRefs.current[0] = el)}
                 />
-                {/* <div className="card-number-1 inter-normal-white-15px">zoe_li</div>
-                <div className="random-dice">
-                  <div className="card-number-2">Random</div>
-                  <img className="ic_casino_24px" src="img/profile/ic-casino-24px@1x.png" alt="ic_casino_24px" />
-                </div> */}
               </div>
               <div className="search_bar-1 search_bar-4">
                 {/* <p className="card-number-3 inter-semi-bold-keppel-15px">
@@ -563,151 +533,3 @@ const AnimaGenContent: React.FunctionComponent<IAnimaGenContentProps> = (props) 
 };
 
 export default AnimaGenContent;
-
-// return (
-//   <div className="bettermidapp-profile-1">
-//     <ShortTitleBar title='Profile' />
-//     <div className="x16212-mYAx6W">
-//         <div className="x888-ySqSNx"></div>
-//         <div className="my-friends-ySqSNx inter-semi-bold-white-18px">My Friends</div>
-//         <div className="view-all-120-ySqSNx inter-medium-royal-blue-14px">View all (120)</div>
-//     </div>
-//     <div className="x4-mYAx6W">
-//         <div className="friends-YkVKQj friends">
-//           <div className="friends-StsdnU friends">
-//               <div className="friend-WmQMWx friend">
-//                 <img className="nft_-avatar-eAUt5U nft_-avatar" src="img/profile/nft-avatar-8@1x.png" alt="NFT_Avatar" />
-//                 <div className="maple inter-normal-white-14px">Maple</div>
-//               </div>
-//               <div className="friend-nxH9CQ friend">
-//                 <img className="nft_-avatar-hxX6ds nft_-avatar" src="img/profile/nft-avatar-9@1x.png" alt="NFT_Avatar" />
-//                 <div className="samantha inter-normal-white-14px">Samantha</div>
-//               </div>
-//               <div className="friend-1KEEE5 friend">
-//                 <img className="nft_-avatar-HKS8zh nft_-avatar" src="img/profile/nft-avatar-10@1x.png" alt="NFT_Avatar" />
-//                 <div className="ronald inter-normal-white-14px">Ronald</div>
-//               </div>
-//               <div className="friend-hnvfxm friend">
-//                 <img className="nft_-avatar-H8e0rI nft_-avatar" src="img/profile/nft-avatar-11@1x.png" alt="NFT_Avatar" />
-//                 <div className="pheobus inter-normal-white-14px">Pheobus</div>
-//               </div>
-//           </div>
-//           <div className="friends-7avldw friends">
-//               <div className="friend-Xg1IZd friend">
-//                 <img className="nft_-avatar-BN9JUR nft_-avatar" src="img/profile/nft-avatar-8@1x.png" alt="NFT_Avatar" />
-//                 <div className="maple inter-normal-white-14px">Maple</div>
-//               </div>
-//               <div className="friend-DkWpJj friend">
-//                 <img className="nft_-avatar-TUw4zm nft_-avatar" src="img/profile/nft-avatar-9@1x.png" alt="NFT_Avatar" />
-//                 <div className="samantha inter-normal-white-14px">Samantha</div>
-//               </div>
-//               <div className="friend-SAkoWC friend">
-//                 <img className="nft_-avatar-ADQV6Y nft_-avatar" src="img/profile/nft-avatar-10@1x.png" alt="NFT_Avatar" />
-//                 <div className="ronald inter-normal-white-14px">Ronald</div>
-//               </div>
-//               <div className="friend-aQA2Yw friend">
-//                 <img className="nft_-avatar-QbW8xx nft_-avatar" src="img/profile/nft-avatar-11@1x.png" alt="NFT_Avatar" />
-//                 <div className="pheobus inter-normal-white-14px">Pheobus</div>
-//               </div>
-//           </div>
-//         </div>
-//     </div>
-//     <div className="x3-mYAx6W">
-//         <div className="add-nft-WdopRj"></div>
-//         <div className="x24-WdopRj">
-//           <div className="x0-hg7RBj"><img className="nft" src="img/full_mimi.png" alt="NFT" /></div>
-//           <div className="x1-hg7RBj"><img className="nft" src="img/full_mimi.png" alt="NFT" /></div>
-//           <div className="x2-hg7RBj"><img className="nft" src="img/full_mimi.png" alt="NFT" /></div>
-//         </div>
-//         <img className="add-WdopRj" src="img/profile/add-1@1x.png" alt="Add" />
-//         <img className="ic_add-WdopRj" src="img/profile/ic-add-1@1x.png" alt="ic_add" />
-//     </div>
-//     <div className="profile-card-mYAx6W">
-//         <img className="layer-YwZqds" src="img/profile/layer-4@1x.png" alt="Layer" />
-//         <Link to={'/myNftList'}>
-//           <div className="button_nft-collections-YwZqds">
-//             <div className="button1-pUSHJN"></div>
-//             <div className="continue-pUSHJN inter-semi-bold-white-15px">My NFTs Collections</div>
-//           </div>
-//         </Link>
-//         <div className="ic_edit-YwZqds ic_edit">
-//           <img className="ic_edit-yzbpuj ic_edit" src="img/profile/ic-edit@1x.png" alt="ic_edit" />
-//         </div>
-//         <div className="ic_add_friends-YwZqds ic_add_friends">
-//           <img className="ic_add_friends-SYgxJR ic_add_friends" src="img/profile/ic-add-friends@1x.png" alt="ic_add_friends" />
-//         </div>
-//         <div className="ic_text-YwZqds ic_text">
-//           <img className="ic_text-rE9IRI ic_text" src="img/profile/ic-text@1x.png" alt="ic_text" />
-//         </div>
-//         <div className="profile-content-YwZqds">
-//           <div className="zoe_li-JYIfVl">{username}</div>
-//           <p className="im-a-positive-person-JYIfVl inter-normal-white-12px">
-//               I'm a positive person. I love to travel and eat.
-//           </p>
-//           <p className="x29-personal-trainer-JYIfVl inter-semi-bold-keppel-14px">
-//               ♉️&nbsp;&nbsp;|&nbsp;&nbsp;29&nbsp;&nbsp;|&nbsp;&nbsp;PERSONAL TRAINER
-//           </p>
-//         </div>
-//         <Link to={'/myNftList'}>
-//           <div className="view-all-YwZqds inter-medium-royal-blue-14px">View all</div>
-//         </Link>
-//         {loading ?
-//         <img className="nft_-avatar-YwZqds nft_-avatar" src="img/profile/nft-avatar-12@1x.png" alt="NFT_Avatar" />
-//         :
-//         (
-//           <img className = "nft_-avatar-YwZqds nft_-avatar" src = {`https://ipfs.io/ipfs/${imgAddress}`}></img>
-//         )
-// }
-//       </div>
-//     <MenuBar/>
-//   </div>
-// )
-
-/* <div className="title-bar">
-        <div className="overlap-group2">
-          <div className="bars-status-bar-i-phone-light">
-            <div className="status-bar">
-              <div className="time-style"><div className="time sfprotext-semi-bold-white-15px">9:41</div></div>
-              <img className="cellular-connection" src="img/profile/cellular-connection-1@1x.png" alt="Cellular Connection" />
-              <img className="wifi" src="img/profile/wifi-1@1x.png" alt="Wifi" />
-              <div className="battery">
-                <div className="overlap-group-1">
-                  <div className="capacity"></div>
-                </div>
-                <img className="cap" src="img/profile/cap-1@1x.png" alt="Cap" />
-              </div>
-            </div>
-          </div>
-          <div className="overlap-group3">
-            <div className="profile inter-semi-bold-white-18px">Profile</div>
-            <div className="overlap-group1-profile-1">
-              <img className="seperat-line" src="img/profile/seperat-line-10@1x.png" alt="Seperat line" />
-              <div className="icon-arrow-left">
-                <img className="icon-arrow-left-1" src="img/profile/icon-arrow-left-1@1x.png" alt="icon-arrow-left" />
-              </div>
-            </div>
-            <div className="ic_settings_24px"></div>
-            <div className="ic_sentiment_very_satisfied_24px"></div>
-          </div>
-        </div>
-      </div> */
-/* <div className="menu-bar">
-        <div className="overlap-group4">
-          <img className="seperat-line-1" src="img/profile/seperat-line-10@1x.png" alt="Seperat line" />
-          <div className="flex-row">
-            <div className="ic_-missions ic_"></div>
-            <div className="ic_-selfie ic_"></div>
-            <div className="ic_home"></div>
-            <div className="ic_rewards">
-              <div className="ic_rewards-1">
-                <img className="x11661" src="img/profile/file---11661@1x.png" alt="11661" />
-                <div className="flex-row-1">
-                  <img className="x116" src="img/profile/file---11659@1x.png" alt="11659" />
-                  <img className="x116" src="img/profile/file---11660@1x.png" alt="11660" />
-                </div>
-              </div>
-            </div>
-            <div className="ic_profile"></div>
-          </div>
-        </div>
-      </div> */
