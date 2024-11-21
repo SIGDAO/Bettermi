e277b8f55dbbfd56339f480b396c1c9a73682d60
joeman <manshunwing@gmail.com>
2024-11-21 08:58:55 +0800

    update the coupon detail page

--- src/redux/couponSystemAPISlice.ts
+++ src/redux/couponSystemAPISlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { couponUserSlice } from "./couponUser";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8082", // "http://localhost:8082", "https://dapp.bettermi.io/couponApi/",
+  baseUrl: "https://dapp.bettermi.io/couponApi/", // "http://localhost:8082", "https://dapp.bettermi.io/couponApi/",
   credentials: 'include', 
   prepareHeaders: (headers, { getState }) => {
     try {
